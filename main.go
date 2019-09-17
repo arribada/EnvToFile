34b962468571ed53dfa1777365165086ca8ba891
Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>
2019-09-17 17:31:18 +0300

    init
    Signed-off-by: Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>

--- main.go
+++ main.go
@@ -20,9 +20,9 @@ func main() {
 	need to use docker images which accept configs only through files.
 	`)
 	app.HelpFlag.Short('h')
-
 	dest := app.Flag("dir", "destination to which it should write the files").
 		Short('d').ExistingDir()
+	kingpin.MustParse(app.Parse(os.Args[1:]))
 
 	for _, env := range os.Environ() {
 		d := strings.Split(env, "=")
@@ -32,7 +32,6 @@ func main() {
 			log.Printf("could not write env var:%v to:%v \n", d[0], destPath)
 		}
 		log.Printf("wrote env var:%v to:%v \n", d[0], destPath)
-
 	}
 	exitSignal := make(chan os.Signal)
 	signal.Notify(exitSignal, syscall.SIGINT, syscall.SIGTERM)
