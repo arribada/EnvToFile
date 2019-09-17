3623505825612922d23d949cf876396789d5dafa
Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>
2019-09-17 17:15:21 +0300

    init
    Signed-off-by: Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/arribada/envToFile
+
+go 1.12
+
+require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20190910110746-680d30ca3117 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/pkg/errors v0.8.1
+	github.com/prometheus/client_golang v1.1.0
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+)
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"strings"
+	"syscall"
+
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+func main() {
+	log.SetFlags(log.Ltime | log.Lshortfile)
+	app := kingpin.New(filepath.Base(os.Args[0]), `
+	A simple tool that reads all env variables and writes each one to a file.
+	Useful in environments where the only way to add configuratios is through env variables and
+	need to use docker images which accept configs only through files.
+	`)
+	app.HelpFlag.Short('h')
+
+	dest := app.Flag("dir", "destination to which it should write the files").
+		Short('d').ExistingDir()
+
+	for _, env := range os.Environ() {
+		d := strings.Split(env, "=")
+		destPath := filepath.Join(*dest, d[0])
+		err := ioutil.WriteFile(destPath, []byte(d[1]), 0644)
+		if err != nil {
+			log.Printf("could not write env var:%v to:%v \n", d[0], destPath)
+		}
+		log.Printf("wrote env var:%v to:%v \n", d[0], destPath)
+
+	}
+	exitSignal := make(chan os.Signal)
+	signal.Notify(exitSignal, syscall.SIGINT, syscall.SIGTERM)
+	<-exitSignal
+}
